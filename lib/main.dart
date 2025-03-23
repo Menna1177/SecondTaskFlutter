7efa842d8808784f9c52dc11cfc5e9de5e45da02
Menna1177 <20231177@stud.fci-cu.edu.eg>
2025-03-23 17:03:12 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,140 +5,81 @@ void main() {
     debugShowCheckedModeBanner: false,
     home: Scaffold(
       backgroundColor: Colors.black,
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
+      body: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+        Container(
+          color: Color(0xff00ff08),
+          child: Row(
+            children: [
+              Image.asset(
+                'images/OIP.jpg',
+                width: 350,
+                height: 200,
+              )
+            ],
+          ),
+        ),
+        SizedBox(
+          height: 60,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Row(
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Your level expertise',
-                      style: TextStyle(color: Color(0xffb9ff11), fontSize: 35),
-                    ),
-                    Text(
-                      'level?',
-                      style: TextStyle(color: Color(0xffb9ff11), fontSize: 35),
-                    )
-                  ],
-                )
-              ],
-            ),
-            SizedBox(height: 30),
-            Row(
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'For the experience and personalised',
-                      style: TextStyle(color: Color(0xffffffff), fontSize: 20),
-                    ),
-                    Text(
-                      'plans for you we need to know your',
-                      style: TextStyle(color: Color(0xffffffff), fontSize: 20),
-                    ),
-                    Text(
-                      'expertise level',
-                      style: TextStyle(color: Color(0xffffffff), fontSize: 20),
-                    )
-                  ],
-                )
-              ],
-            ),
-            SizedBox(height: 80),
             Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Container(
-                  width: 200,
-                  height: 50,
-                  decoration: BoxDecoration(
-                    color: Color(0xff6e6e6e),
-                    borderRadius: BorderRadius.circular(20),
-                    border: Border.all(
-                      color: Color(0xffb9ff11),
-                      width: 2,
-                    ),
-                  ),
-                  child: Center(
-                    child: Text(
-                      'Beginner',
-                      style: TextStyle(
-                          color: Color(0xffb9ff11),
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold),
-                    ),
-                  ),
+                Text(
+                  'ZenFit',
+                  style: TextStyle(color: Colors.white, fontSize: 40),
                 ),
-                SizedBox(height: 20),
-                Container(
-                  width: 200,
-                  height: 50,
-                  decoration: BoxDecoration(
-                    color: Color(0xff6e6e6e),
-                    borderRadius: BorderRadius.circular(20),
-                    border: Border.all(
-                      color: Color(0xffb9ff11),
-                      width: 2,
-                    ),
-                  ),
-                  child: Center(
-                    child: Text(
-                      'Intermediate',
-                      style: TextStyle(
-                          color: Color(0xffb9ff11),
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold),
-                    ),
-                  ),
+                SizedBox(
+                  height: 20,
                 ),
-                SizedBox(height: 20),
-                Container(
-                  width: 200,
-                  height: 50,
-                  decoration: BoxDecoration(
-                    color: Color(0xff6e6e6e),
-                    borderRadius: BorderRadius.circular(20),
-                    border: Border.all(
-                      color: Color(0xffb9ff11),
-                      width: 2,
-                    ),
-                  ),
-                  child: Center(
-                    child: Text(
-                      'Advanced',
-                      style: TextStyle(
-                          color: Color(0xffb9ff11),
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 80),
-                Container(
-                  width: 150,
-                  height: 50,
-                  decoration: BoxDecoration(
-                    color: Color(0xffb9ff11),
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: Center(
-                    child: Text(
-                      'Next',
-                      style: TextStyle(
-                          color: Color(0xff000000),
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                ),
+                Text('Unleash Your potential through',
+                    style: TextStyle(color: Colors.white, fontSize: 20)),
+                Text('movement',
+                    style: TextStyle(color: Colors.white, fontSize: 20)),
               ],
             )
           ],
         ),
-      ),
+        SizedBox(
+          height: 80,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 150,
+              height: 50,
+              decoration: BoxDecoration(
+                color: Color(0xff00ff81),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Let\'s do it!',
+                    style: TextStyle(
+                        color: Colors.black, fontSize: 20), // Text color
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Skip for noe',
+              style: TextStyle(color: Colors.white, fontSize: 15), // Text color
+            ),
+          ],
+        )
+      ]),
     ),
   ));
 }
